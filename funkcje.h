fbad25d4b6dea2f5e10c0900fc99935395d0a379
axilan <krulgier@gmail.com>
2021-03-05 11:28:15 +0100

    It's still working, but do nothing

---
+++ funkcje.h
@@ -0,0 +1,15 @@
+//Series of crucial functions and variables to Todo file and 
+Funkcje {
+Public:
+//To add line of text to To Do file;
+void Add_File();
+XD
+//Change task description, name or finish date;
+void Edit_Task();
+
+//Change status in task from active to finish or delete it completely;
+void Finish_Task();
+
+//Show all not finished tasks;
+void Show_Tasks();
+};
