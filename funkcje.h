3e185e46e20a4e009067e190d7080e53238b5231
axilan <krulgier@gmail.com>
2021-03-05 12:24:13 +0100

    forgoten to use git add before

--- funkcje.h
+++ funkcje.h
@@ -1,14 +1,17 @@
 //Series of crucial functions and variables to Todo file and 
-Funkcje {
-Public:
+class Funkcje {
+public:
 //To add line of text to To Do file;
 void Add_File();
-XD
+
 //Change task description, name or finish date;
 void Edit_Task();
 
 //Change status in task from active to finish or delete it completely;
 void Finish_Task();
+
+//delete task from file;
+void Deleate_Task();
 
 //Show all not finished tasks;
 void Show_Tasks();
--- main.cpp
+++ main.cpp
@@ -3,7 +3,30 @@
 using namespace std;
 int main()
 {
-	cout<<"Hello world";
+	int Odp; //var that choose with one case choose
+	setlocale( LC_ALL, "" ); //This allow me to use polich characters;
+	
+	//Looping a main function in order to keep aplication working until a user choose to exit;
+	do {
+	cout<<"Witaj, jak¹ akcjê chcesz wykonaæ?: "<<endl;
+	cout<<"1. Wyœwietl zadania"<<endl;
+	cout<<"2. Edytuj zadanie"<<endl;
+	cout<<"3. Dodaj zadanie"<<endl;
+	cout<<"4. Zakoñcz zadanie"<<endl;
+	cout<<"5. Usuñ zadanie"<<endl;
+	cout<<"6. Zakoñcz dzia³anie programu"<<endl;
+
+	cin>> Odp;
+	
+	switch(Odp){
+		case 1: {cout<<"Wyswietlanie"; break;}
+		case 2: {cout<<"Edytowanie"; break;}
+		case 3: {cout<<"Dodawanie"; break;}
+		case 4: {cout<<"Zakañczanie"; break;}
+		case 5: {cout<<"Usuwanie"; break;}
+		case 6: {cout<<"Wychodzenie"; break;}
+	}
 	getchar();
+	} while(Odp!=6);
 	return 0;
 }
